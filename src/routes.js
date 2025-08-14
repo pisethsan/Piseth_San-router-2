1806e926b257bc73cb82800685313410474a903f
Piseth San <pisethsan67167@gmail.com>
2025-08-14 10:37:23 +0700

    up register

--- src/routes.js
+++ src/routes.js
@@ -14,7 +14,7 @@ const routes = [
     { path: '/contact', component: Contact },
     { path: '/project', component: Project },
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
-    // { path: '/register', component: Register },
+    { path: '/register', component: Register },
 ]
 
 const router = createRouter({
